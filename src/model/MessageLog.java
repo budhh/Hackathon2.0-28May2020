4778707e1103da5a022820c902413aa8462120a7
Abdullah <abdullahharoon1632002@outlook.com>
2021-05-28 15:28:22 -0700

    Added Main, MessageLog and User

--- src/model/MessageLog.java
+++ src/model/MessageLog.java
@@ -1,4 +1,12 @@
 package model;
 
+import java.util.HashMap;
+
 public class MessageLog {
+
+    HashMap<User, Message> messageLog;
+
+    MessageLog(HashMap log) {
+        this.messageLog = log;
+    }
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,14 @@
 package model;
 
 public class User {
+
+    String username;
+
+    public User(String name) {
+        this.username = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
