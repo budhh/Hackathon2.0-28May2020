221bc8513edc5fe32bd2a6c2f32cba1dc74aa9e2
Crec0 <83436716+Crec0@users.noreply.github.com>
2021-05-28 15:17:05 -0700

    Implemented message and user classes

--- src/model/Message.java
+++ src/model/Message.java
@@ -1,4 +1,55 @@
 package model;
 
+
+import java.time.Instant;
+import java.util.Objects;
+
 public class Message {
+
+    private final String message;
+    private final User user;
+    private final Instant instant;
+
+    Message(String message, User user) {
+        this.message = message;
+        this.user = user;
+        this.instant = Instant.now();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public User getUser() {
+        return user.copy();
+    }
+
+    public Instant getInstant() {
+        return instant;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message1 = (Message) o;
+        return message.equals(message1.message) &&
+                user.equals(message1.user) &&
+                instant.equals(message1.instant);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message, user, instant);
+    }
+
+    @Override
+    public String toString() {
+
+        return "Message{" +
+                "message='" + message + '\'' +
+                ", user=" + user +
+                ", instant=" + instant +
+                '}';
+    }
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,41 @@
 package model;
 
+import java.util.Objects;
+
 public class User {
+
+    private final String client;
+
+    User(String client) {
+        this.client = client;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public User copy() {
+        return (new User(this.client));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return client.equals(user.client);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(client);
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "client='" + client + '\'' +
+                '}';
+    }
 }
